594a30d19de121fbc25c43affec82eb0ecc1c666
Hamza <124891005+HyHamza@users.noreply.github.com>
2024-03-18 07:32:23 +0000

    Updated config,js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 global.audio= "" ;  
 global.video= "" ;
 global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""  // put your app url here,
-global.email ="samsamsun789@gmail.com"
+global.appUrl=process.env.APP_URL || "https://Talkdrove.cc.nf/"  // put your app url here,
+global.email ="qulzam378@gmail.com"
 global.location="Lahore,Pakistan."
 
 
@@ -19,16 +19,16 @@ global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URI = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
-global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  = process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website= process.env.GURL|| "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
+global.github=process.env.GITHUB|| "";
+global.gurl  = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l";
+global.website= process.env.GURL|| "https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
-global.devs = "923184474176" // Developer Contact
+global.devs = "923072380380" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923184474176";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923072380380";
 
 
 
@@ -45,41 +45,41 @@ global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in g
 
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
 global.userImages= process.env.USER_IMAGES|| "text", // set IMAGE/VIDEO links here
-global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
+global.waPresence= process.env.WAPRESENCE ||  "unavailable" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "923184474176,923xxxxxxxx";
+global.readmessage = process.env.READ_MESSAGE || "true"
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "923072380380,923xxxxxxxx";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923072380380,923xxxxxxxx";
 
 global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_08_56_03_14_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiVUxUaHd4M0tETjJNUU83OE43VmpRd3JjZ1Z1ZFlVSVV2N1A2SFlHMEpsVT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIk9PU1ZBMEhqZFk4SnpHMEl1ZjQwRUxuSmxzdmR5MFhSN01ySnVXTWlwWFE9XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiNkgvSzlsMXd0LythOFc0M3JlQnpsVFdDaXFwYm14T2h6azBMZUhVbFpXTT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcInhpUURkNDRaVVVSQXVCdmNyVVR6RHNHVkljbXo4d3hyUG1DZHdmZzByamc9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiaUU4eXEzWW1GTjdkY2VQTjVDU05NalBBNkNXQmxZNVkwdmJpZXdlQ3hWND1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImR1N3RXa05NZVQyb2pwMkVNeTlORWJ1WGdNZTNsdm1seXhDbFg5am54SHc9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJFRzFuWHA3di9pbXk4ckZSbkNlSXJNTkpUZGh2dFRZOVo3U3Y0SzNBWG1jPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiR21uYWdkbFBpMzBxek9NUXkzZllYLzUxNzRBRmI2N1RXa3dVTVFyaDBBOD1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJtbmprQWN6eEtqSzFHL0E2bzRlcHFHQ29rTGZyRFh4REtsUkw2ZUlDbGR5K0JmZVpPczdSdy9zWWsyWGs1L1pmSlR0TmFOaUY5UXFITVJuRExqOVJnZz09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6MjM3LFwiYWR2U2VjcmV0S2V5XCI6XCI5dTBuUGlFbXBVZ0dZNlZ3VndpWjZMZXVNVVdnenJEUjNPTGFFMkZoNW5NPVwiLFwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzXCI6W10sXCJuZXh0UHJlS2V5SWRcIjozMixcImZpcnN0VW51cGxvYWRlZFByZUtleUlkXCI6MzIsXCJhY2NvdW50U3luY0NvdW50ZXJcIjowLFwiYWNjb3VudFNldHRpbmdzXCI6e1widW5hcmNoaXZlQ2hhdHNcIjpmYWxzZX0sXCJkZXZpY2VJZFwiOlwiVEZ0QmpKal9TMXVVcXV6V0R5WFBud1wiLFwicGhvbmVJZFwiOlwiMjgzMjhhN2ItODg2Ny00ZjhiLTllNDEtYTZhMTFjMTE0MDdkXCIsXCJpZGVudGl0eUlkXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJwTXVlaXJHSllDbnBIQXN5M1psSTMzMEFVUkE9XCJ9LFwicmVnaXN0ZXJlZFwiOnRydWUsXCJiYWNrdXBUb2tlblwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiRXN4dDdkRVp0TFNnQVhSVVBOcEpicUo1Znd3PVwifSxcInJlZ2lzdHJhdGlvblwiOnt9LFwicGFpcmluZ0NvZGVcIjpcIkJON1FaNzlNXCIsXCJtZVwiOntcImlkXCI6XCI5MjMwNzIzODAzODA6MzZAcy53aGF0c2FwcC5uZXRcIixcImxpZFwiOlwiMTUzNjYxMDc4NTQ0NTE1OjM2QGxpZFwiLFwibmFtZVwiOlwiOylcIn0sXCJhY2NvdW50XCI6e1wiZGV0YWlsc1wiOlwiQ0pqK2o2WUJFTVAzeXE4R0dBc2dBQ2dBXCIsXCJhY2NvdW50U2lnbmF0dXJlS2V5XCI6XCJoTUtYM1QwMHdUbFUvZGxjRXJvK0xzZ1V0bW9JRHNPdXdVQzh3b0F0NG0wPVwiLFwiYWNjb3VudFNpZ25hdHVyZVwiOlwicjl5OVQ5aDJQZjRualE0MWhmR2JxVmN4Z3ZWN1k1Q2xuNWFWMHhSVjE2bktGZzBTQmRydXkwQ0JYaWM0dDlTeHZMcm9BN01OOSthTys1bXVFbjBXQ2c9PVwiLFwiZGV2aWNlU2lnbmF0dXJlXCI6XCJLVU5qV1JmaUxjYVEvMlZTTGFIUCtZOU04TXJwbnVIV1gzV0h4V3B0ZHAraHZkYUE2TDdFYTlSdUZqSkdmcEJ5azg3TzdWbUZYa20zMHROK05vWk1qdz09XCJ9LFwic2lnbmFsSWRlbnRpdGllc1wiOlt7XCJpZGVudGlmaWVyXCI6e1wibmFtZVwiOlwiOTIzMDcyMzgwMzgwOjM2QHMud2hhdHNhcHAubmV0XCIsXCJkZXZpY2VJZFwiOjB9LFwiaWRlbnRpZmllcktleVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQllUQ2w5MDlOTUU1VlAzWlhCSzZQaTdJRkxacUNBN0Ryc0ZBdk1LQUxlSnRcIn19XSxcInBsYXRmb3JtXCI6XCJhbmRyb2lkXCIsXCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXBcIjoxNzEwNDA2NTk5LFwibXlBcHBTdGF0ZUtleUlkXCI6XCJBQUFBQUVDTVwifSIsCiAgImFwcC1zdGF0ZS1zeW5jLWtleS1BQUFBQUVDTS5qc29uIjogIntcImtleURhdGFcIjpcInQ1OVJCejNVL3JyL1V2QXNFV1RxY29uaWpab0RsZGtmRU9oUkVyVG1mQVk9XCIsXCJmaW5nZXJwcmludFwiOntcInJhd0lkXCI6MzQ4Mzg5MTQ0LFwiY3VycmVudEluZGV4XCI6MSxcImRldmljZUluZGV4ZXNcIjpbMCwxXX0sXCJ0aW1lc3RhbXBcIjpcIjE3MTAxMzc5MzQ0MjRcIn0iCn0=" ;
 
 
 module.exports = {
 
   menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
-  HANDLERS: process.env.PREFIX  || ".",
+  HANDLERS: process.env.PREFIX  || ",",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.0",
-  caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "POWERED BY Hamza" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
  
-  author : process.env.PACK_AUTHER|| "Suhail-MD",
+  author : process.env.PACK_AUTHER|| "Byte",
   packname: process.env.PACK_NAME || "♥️",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "It'x Suhail",
+  botname : process.env.BOT_NAME  || "Byte",
+  ownername:process.env.OWNER_NAME|| "Byte",
 
 
   errorChat : process.env.ERROR_CHAT || "",
@@ -94,7 +94,7 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
+  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "public",
   LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "SUHAIL",
 
 
